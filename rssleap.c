2860d763bc2c7f2085bc14470bc2463a4efa6faf
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-07-23 13:17:00 +0530

    Create rssleap.c

---
+++ rssleap.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int y;
+printf("enter the year");
+scanf("%d",&y);
+{
+if(y%4==0)
+   {
+     if(y%100==0)
+     {
+      if(y%400==0)
+       {
+          printf("is a leap year");
+       }
+      else
+      {
+        printf("is not a leap year");
+      }
+   { 
+    printf("is a leap year");
+   }
+   else
+   {
+   printf("is not a leap year");
+   }
+{
+printf("is a leap year");
+}
+else
+{
+printf("is not a leap year");
+}
+}
+}
